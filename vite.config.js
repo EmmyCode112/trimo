70db0d8383bb270f96e32e86980646cfc15f0168
EmmyCode112 <emmywiser3@gmail.com>
2025-02-01 09:19:40 +0100

    pushing for hosting

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/trimo", // Replace "REPO_NAME" with your actual repository name
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Alias "@" to the "src" directory
     },
   },
-})
+});
